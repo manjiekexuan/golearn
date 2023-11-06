099c096555295a27f4ed99bd2c62cabe573918f3
薛冶 <xueye@xueyedeMacBook-Pro.local>
2023-11-06 14:26:38 +0800

    修正更新202311061426

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	counts := make(map[string]int)
 
-	input := bufio.NewScanner(os.Stdin)
+	fmt.Printf("ABC")
+	os.Exit(1)
 
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
 }
